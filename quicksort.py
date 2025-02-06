fdae1e4cb373c70a683a655e2d3b02ab31a25795
Pirran Hyde-Thomas <81690719+Daft396@users.noreply.github.com>
2025-02-06 13:14:53 +0000

    Updated algorithm
    Updated algorithm to handle non unique numbers and added a generate array function and time taken variable

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,14 @@
-arr = [2,3,5,7,6,4,778,54]
+import random
+import time
+starttime = time.time()
+arr = []
+def generate_array(arr):
+    elmnts = int(input("Enter the amount of numbers: "))
+    lowerbound = int(input("Enter the lower bound of your numbers: "))
+    upperbound = int(input(f"Enter the range of your numbers, from {lowerbound} to: "))
+    for i in range(elmnts):
+        arr.append(random.randint(lowerbound,upperbound))
+    return arr
 def qsort(arr):
     if len(arr) <= 1:
         return arr
@@ -11,10 +21,10 @@ def qsort(arr):
         while left< right and arr[left] < pivot:
             left+=1
 
-        while right >=left and arr[right] > pivot:
+        while right >=left and arr[right] >= pivot:
             right -= 1
 
-        if left < right:
+        if left <= right:
             temp = arr[left]
             arr[left] =arr[right]
             arr[right] = temp
@@ -26,4 +36,8 @@ def qsort(arr):
     left_list = qsort(arr[0:(right)])
     right_list = qsort(arr[right+1:(len(arr))])
     return left_list + [arr[right]] + right_list
+generate_array(arr)
 print(qsort(arr))
+endtime = time.time()
+timetaken = endtime - starttime
+print(f"This sorting algorithm took {timetaken} seconds")
